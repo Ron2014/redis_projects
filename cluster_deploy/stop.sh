39215fa96b77a092b0da4c3115968111db3e6467
root <root@localhost.localdomain>
2021-10-02 02:08:08 +0800

    files

--- cluster_deploy/stop.sh
+++ cluster_deploy/stop.sh
@@ -3,7 +3,7 @@
 source ./conf.ini
 
 for REDIS_PORT in $(ls $ROOTDIR); do
-redis-cli -h ${REDIS_HOST} -p ${REDIS_PORT} -a ${REDIS_PSWD} -c shutdown
+redis-cli -h ${LOCAL_HOST} -p ${REDIS_PORT} -a ${REDIS_PSWD} -c shutdown
 if [ 0 -eq $? ]; then
 echo -e "\033[01;32mstop redis" $REDIS_PORT "success! \033[00m"
 fi
